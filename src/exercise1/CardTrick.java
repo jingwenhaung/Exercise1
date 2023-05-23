7711fc3786c3216ac8ea65815dbfa7c548ca5fa3
jingwenhaung <133809176+jingwenhaung@users.noreply.github.com>
2023-05-23 15:57:32 -0400

    Update CardTrick.java
    add my information

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -63,19 +63,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Jingwen, but you can call me Missy");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Finish the Computer Programing");
+        System.out.println("-- Find a job about coding!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Traveling");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Hiking");
 
         System.out.println();
         
